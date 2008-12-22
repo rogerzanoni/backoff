233ff25fe2427b1b5294abffb5e028490dd980ce
cavalcanti.luiz <cavalcanti.luiz@c00ee482-cdfe-11dd-b6f5-21ad4398c266>
2008-12-22 07:57:53 +0000

    layered map and it's XML represetation prototype

---
+++ backoff-engine/src/com/deadtroll/backoff/engine/map/MapBlock.java
@@ -0,0 +1,17 @@
+package com.deadtroll.backoff.engine.map;
+
+import org.newdawn.slick.Image;
+
+public class MapBlock {
+
+	private Image sprite;
+
+	public Image getSprite() {
+		return sprite;
+	}
+
+	public void setSprite(Image sprite) {
+		this.sprite = sprite;
+	}
+
+}
--- backoff/src/com/deadtroll/backoff/Main.java
+++ backoff/src/com/deadtroll/backoff/Main.java
@@ -9,7 +9,8 @@ public class Main {
 		try { 
 		    AppGameContainer container = new AppGameContainer(new BackOffGame());
 		    container.setDisplayMode(BackOffGame.GAME_WIDTH, BackOffGame.GAME_HEIGHT,false);
-		    container.setMinimumLogicUpdateInterval(1000/30);
+		    container.setMinimumLogicUpdateInterval(1000/20);
+		    container.setMaximumLogicUpdateInterval(1000/30);
 		    container.start();
 		} catch (SlickException e) { 
 		    e.printStackTrace(); 
