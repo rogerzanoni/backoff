a315433e6dce50227ddaa3d5fa7541794c2f1c57
cavalcanti.luiz <cavalcanti.luiz@c00ee482-cdfe-11dd-b6f5-21ad4398c266>
2009-01-02 01:54:42 +0000

    Map editor improvements

---
+++ trunk/backoff-engine/src/com/deadtroll/backoff/engine/map/MapIOUtil.java
@@ -0,0 +1,113 @@
+package com.deadtroll.backoff.engine.map;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.InternetHeaders;
+
+
+public class MapIOUtil {
+
+	public static Map loadMap(String src) throws Throwable {
+		Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new File(src));
+		Map map = xmlToMap(doc);
+		return map;
+	}
+	
+	public static void saveMap(Map map, String dst) throws Exception {
+		Document mapDocument = mapToXml(map);
+		String output = xmlToString(mapDocument);
+		FileOutputStream fos = new FileOutputStream(new File(dst));
+		fos.write(output.getBytes());
+		fos.close();
+	}
+	
+	private static Map xmlToMap(Document doc) {
+		Map map = new Map();
+		Element root = doc.getDocumentElement();
+		map.setDescription(root.getAttribute("description"));
+		map.setSpriteSheet(root.getAttribute("spriteSheet"));
+		map.setSpriteSheetWidth(Integer.parseInt(root.getAttribute("spriteWidth")));
+		map.setSpriteSheetHeight(Integer.parseInt(root.getAttribute("spriteHeight")));
+		map.setMapWidth(Integer.parseInt(root.getAttribute("width")));
+		map.setMapHeight(Integer.parseInt(root.getAttribute("height")));
+		map.setPlayerLayer(Integer.parseInt(root.getAttribute("playerLayer")));
+		map.setLayers(new MapLayer[root.getChildNodes().getLength()]);
+		
+		for (int i=0; i<root.getChildNodes().getLength(); i++) {
+			Element layerElement = (Element)root.getChildNodes().item(i);
+			MapLayer ml = new MapLayer();
+			ml.setId(layerElement.getAttribute("id"));
+			ml.setZOrder(Integer.parseInt(layerElement.getAttribute("zOrder")));
+			ml.setMatrix(new MapBlock[map.getMapWidth()][map.getMapHeight()]);
+			for (int j=0; j<layerElement.getChildNodes().getLength(); j++) {
+				Element blockElement = (Element)layerElement.getChildNodes().item(j);
+				MapBlock mb = new MapBlock();
+				mb.setSpriteId(Integer.parseInt(blockElement.getAttribute("sprite")));
+				int x = Integer.parseInt(blockElement.getAttribute("x"));
+				int y = Integer.parseInt(blockElement.getAttribute("y"));
+				ml.getMatrix()[x][y] = mb;
+			}
+			map.getLayers()[i] = ml;
+		}
+		return map;
+	}
+	
+	private static Document mapToXml(Map map) throws Exception {
+		Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+		Element rootNode = doc.createElement("map");
+		rootNode.setAttribute("description", map.getDescription());
+		rootNode.setAttribute("spriteSheet", map.getSpriteSheet());
+		rootNode.setAttribute("spriteWidth", ""+map.getSpriteSheetWidth());
+		rootNode.setAttribute("spriteHeight", ""+map.getSpriteSheetHeight());
+		rootNode.setAttribute("width", ""+map.getMapWidth());
+		rootNode.setAttribute("height", ""+map.getMapHeight());
+		rootNode.setAttribute("playerLayer", ""+map.getPlayerLayer());
+		
+		int zOrder = 0; 
+		for (MapLayer ml : map.getLayers()) {
+			Element layerNode = doc.createElement("layer");
+			layerNode.setAttribute("id", ml.getId());
+			layerNode.setAttribute("zOrder", ""+zOrder);
+			for (int i=0; i<map.getMapWidth(); i++) {
+				for (int j=0; j<map.getMapHeight(); j++) {
+					MapBlock mb = ml.getMatrix()[i][j];
+					if (mb!=null) {
+						Element blockElement = doc.createElement("block");
+						blockElement.setAttribute("x", ""+i);
+						blockElement.setAttribute("y", ""+j);
+						blockElement.setAttribute("sprite", ""+mb.getSpriteId());
+						layerNode.appendChild(blockElement);
+					}
+				}
+			}
+			rootNode.appendChild(layerNode);
+			zOrder++;
+		}
+		doc.appendChild(rootNode);		
+		return doc;
+	}
+	
+	public static String xmlToString(Document doc) throws Exception {
+	    Source source = new DOMSource(doc.getDocumentElement());
+	    StringWriter stringWriter = new StringWriter();
+	    Result result = new StreamResult(stringWriter);
+	    TransformerFactory factory = TransformerFactory.newInstance();
+	    Transformer transformer = factory.newTransformer();
+	    transformer.transform(source, result);
+	    return stringWriter.getBuffer().toString();
+	}
+	
+}
--- trunk/backoff-map-editor/src/com/deadtroll/backoff/mapeditor/MapCanvas.java
+++ trunk/backoff-map-editor/src/com/deadtroll/backoff/mapeditor/MapCanvas.java
@@ -3,12 +3,17 @@ package com.deadtroll.backoff.mapeditor;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
+
+import com.deadtroll.backoff.engine.map.Map;
+import com.deadtroll.backoff.engine.map.MapBlock;
 
 public class MapCanvas extends JPanel {
 
@@ -17,6 +22,7 @@ public class MapCanvas extends JPanel {
 	private int horizontalBlocks;
 	private int verticalBlocks;
 	private Point currentMouseLocation;
+	private Map map;
 
 	public MapCanvas(int horizontalBlocks, int verticalBlocks) {
 		this.horizontalBlocks = horizontalBlocks;
@@ -40,7 +46,8 @@ public class MapCanvas extends JPanel {
 		
 		this.addMouseListener(new MouseListener() {
 
-			public void mouseClicked(MouseEvent arg0) {
+			public void mouseClicked(MouseEvent evt) {
+				MapCanvas.this.processClick(evt.getButton()==MouseEvent.BUTTON3);
 			}
 
 			public void mouseEntered(MouseEvent arg0) {
@@ -60,6 +67,37 @@ public class MapCanvas extends JPanel {
 		});
 	}
 
+	protected void processClick(boolean erase) {
+		if (MapCanvas.this.map!=null) {
+			int blockWidth = (int)(this.getWidth()/this.horizontalBlocks);
+			int blockHeight = (int)(this.getHeight()/this.verticalBlocks);
+			if (this.currentMouseLocation!=null) {
+				for (int i=0; i<this.horizontalBlocks; i++) {
+					if (this.currentMouseLocation.getX()>(blockWidth*(i))
+							&& this.currentMouseLocation.getX()<(blockWidth*(i+1))) {
+						for (int j=0; j<this.verticalBlocks; j++) {
+							if (this.currentMouseLocation.getY()>(blockHeight*(j))
+									&& this.currentMouseLocation.getY()<(blockHeight*(j+1))) {
+								
+								Map m = ApplicationController.getInstance().getCurrentMap();
+								int layerIndex = ApplicationController.getInstance().getCurrentLayer();
+								int spriteIndex = ApplicationController.getInstance().getCurrentSpriteIndex();
+								MapBlock mb = null;
+								if (!erase) {
+									mb = new MapBlock();
+									mb.setSpriteId(spriteIndex);
+								}
+								m.getLayers()[layerIndex].getMatrix()[i][j] = mb;	
+								break;
+							}
+						}
+						break;
+					}
+				}
+			}
+		}		
+	}
+
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
@@ -77,24 +115,47 @@ public class MapCanvas extends JPanel {
 			g.drawLine(0, blockHeight*(i+1), this.getWidth(), blockHeight*(i+1));
 		}
 
-		if (this.currentMouseLocation!=null) {
+		Map m = ApplicationController.getInstance().getCurrentMap();
+		int layerIndex = ApplicationController.getInstance().getCurrentLayer();
+		if (m!=null) {
 			for (int i=0; i<this.horizontalBlocks; i++) {
-				if (this.currentMouseLocation.getX()>(blockWidth*(i))
-						&& this.currentMouseLocation.getX()<(blockWidth*(i+1))) {
-					for (int j=0; j<this.verticalBlocks; j++) {
-						if (this.currentMouseLocation.getY()>(blockHeight*(j))
-								&& this.currentMouseLocation.getY()<(blockHeight*(j+1))) {
-							g.setColor(Color.GRAY);
-							g.fillRect(blockWidth*(i), blockHeight*(j), blockWidth, blockHeight);
-							break;
-						}
+				for (int j=0; j<this.verticalBlocks; j++) {
+					MapBlock mb = m.getLayers()[layerIndex].getMatrix()[i][j];
+					if (mb!=null) {
+						BufferedImage img = ApplicationController.getInstance().getSpriteMap().get(mb.getSpriteId());
+						Image scaled = img.getScaledInstance(blockWidth, blockHeight, Image.SCALE_FAST);
+						
+						g.drawImage(scaled,blockWidth*(i), blockHeight*(j),null);
 					}
-					break;
 				}
 			}
 		}
 	}
-	
+
+	public int getHorizontalBlocks() {
+		return horizontalBlocks;
+	}
+
+	public void setHorizontalBlocks(int horizontalBlocks) {
+		this.horizontalBlocks = horizontalBlocks;
+	}
+
+	public int getVerticalBlocks() {
+		return verticalBlocks;
+	}
+
+	public void setVerticalBlocks(int verticalBlocks) {
+		this.verticalBlocks = verticalBlocks;
+	}
+
+	public Map getMap() {
+		return map;
+	}
+
+	public void setMap(Map map) {
+		this.map = map;
+	}
+
 //	public void setCursorImage(Image img) {
 //		Toolkit toolkit = Toolkit.getDefaultToolkit();
 //		Point hotSpot = new Point(0,0);
