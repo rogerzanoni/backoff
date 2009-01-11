87c7ce097ebac9b421cc55e0002b2c77ce9c8ac9
rogerzanoni <rogerzanoni@c00ee482-cdfe-11dd-b6f5-21ad4398c266>
2009-01-11 03:15:49 +0000

    - Criação das armas do jogo - Controle de balas no pente - Controle de munição total - Controle de tiros por segundo baseado no firerate da arma

---
+++ backoff-engine/src/com/deadtroll/backoff/engine/weapon/DesertEaglePistol.java
@@ -0,0 +1,14 @@
+package com.deadtroll.backoff.engine.weapon;
+
+public class DesertEaglePistol extends Weapon {
+	public DesertEaglePistol() {
+		setDamage(4);
+		setMagazineSize(10);
+		setMaxAmmo(30);		
+		setFireRate(1.5);
+		setRealoadTime(3);
+		setName("Desert Eagle");
+		setAmmo(0);
+	}
+
+}
