68a4bed6e68a37707bd0aee236617f269a68fac0
cavalcanti.luiz <cavalcanti.luiz@c00ee482-cdfe-11dd-b6f5-21ad4398c266>
2009-07-27 02:05:40 +0000

    Unificação de collisionshape de objetos

--- backoff-engine/src/com/deadtroll/backoff/engine/model/IGameObject.java
+++ backoff-engine/src/com/deadtroll/backoff/engine/model/IGameObject.java
@@ -3,6 +3,7 @@ package com.deadtroll.backoff.engine.model;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SpriteSheet;
+import org.newdawn.slick.geom.Shape;
 import org.newdawn.slick.geom.Vector2f;
 
 import com.deadtroll.backoff.engine.sound.ISoundEventListener;
@@ -12,47 +13,68 @@ import com.deadtroll.backoff.engine.viewport.ViewPort;
 
 public interface IGameObject {
 
-	public float getRotation();	
+	public float getRotation();
+
 	public void setRotation(float angle);
-	
+
 	public void setLayer(int layer);
+
 	public int getLayer();
 
 	public void setPosition(Vector2f position);
-	public Vector2f getPosition();	
+
+	public Vector2f getPosition();
+
 	public Vector2f getCenter();
 
 	public void setDebugMode(boolean debugMode);
+
 	public boolean isDebugMode();
 
 	public void setSpriteSheet(SpriteSheet spriteSheet);
+
 	public Image getCurrentSprite();
+
 	public void render(Graphics g, ViewPort viewPort);
 
 	public void initializeGO();
+
 	public void finalizeGO();
-	
+
 	public void setSpeed(Vector2f speed);
+
 	public Vector2f getSpeed();
-	
+
 	public void setAngle(Vector2f angle);
+
 	public Vector2f getAngle();
-	
+
 	public void setScore(int score);
+
 	public int getScore();
-	
+
 	public void setStatus(TransientStatus status);
-	TransientStatus getStatus();
-	
+
+	public TransientStatus getStatus();
+
 	public void setEnergy(int energy);
+
 	public int getEnergy();
-	
+
 	public void setDamage(int damage);
+
 	public void addDamage(int damage);
+
 	public int getDamage();
-	
-	void addSoundEvents();
-	void setSoundEventListener(ISoundEventListener listener);
-	void removeSoundEvents();
+
+	public void addSoundEvents();
+
+	public void setSoundEventListener(ISoundEventListener listener);
+
+	public void removeSoundEvents();
+
 	public void playSoundEvent(SoundEventType evtType, SoundSequenceType sequenceType, boolean exclusive, boolean interrupt);
+	
+	public Shape getCollisionShape(ViewPort viewport);
+	
 }
