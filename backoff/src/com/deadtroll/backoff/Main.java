805987e610ec1a9add3d17711446d34379245488
cavalcanti.luiz <cavalcanti.luiz@c00ee482-cdfe-11dd-b6f5-21ad4398c266>
2009-08-18 01:14:15 +0000

    Melhoria na colisão e ajuste do projeto backoff ao padrão de scenes.

--- backoff/src/com/deadtroll/backoff/Main.java
+++ backoff/src/com/deadtroll/backoff/Main.java
@@ -3,13 +3,12 @@ package com.deadtroll.backoff;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
 
-
 public class Main {
 
 	public static void main(String[] args) {
 		try { 
 		    AppGameContainer container = new AppGameContainer(new BackOffGame());
-		    container.setDisplayMode(BackOffGame.GAME_WIDTH, BackOffGame.GAME_HEIGHT,false);
+		    container.setDisplayMode(GameScene.GAME_WIDTH,GameScene.GAME_HEIGHT,false);
 		    container.setMinimumLogicUpdateInterval(1000/20);
 		    container.setMaximumLogicUpdateInterval(1000/30);
 		    container.start();
